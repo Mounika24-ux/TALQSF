b3a00163003d67b745439f1f7d296a549f094ef8
Mounika24-ux <pmounikamudhiraj024@gmail.com>
2025-09-14 08:53:42 +0530

    Update main.jsx

--- working_tailwind_react/src/main.jsx
+++ working_tailwind_react/src/main.jsx
@@ -2,9 +2,12 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { DarkModeProvider } from "./contexts/DarkModeContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DarkModeProvider>
+      <App />
+    </DarkModeProvider>
   </React.StrictMode>
 );
